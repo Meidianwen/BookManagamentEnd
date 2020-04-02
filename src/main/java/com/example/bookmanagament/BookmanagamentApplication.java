bc61574cdc554dc17d1eb0d93b612dd459fa8a89
Meidianwen <17863110082@163.com>
2020-04-02 22:12:54 +0800

    project init

---
+++ src/main/java/com/example/bookmanagament/BookmanagamentApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookmanagament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmanagamentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookmanagamentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bookmanagament/BookmanagamentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookmanagament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmanagamentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
